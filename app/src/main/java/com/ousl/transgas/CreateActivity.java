28343f8caf9bc89032895b2eab2a48932c44bda9
zakir <mtm.zakir@gmail.com>
2023-04-10 21:53:43 +0530

    6

--- app/src/main/java/com/ousl/transgas/CreateActivity.java
+++ app/src/main/java/com/ousl/transgas/CreateActivity.java
@@ -2,13 +2,26 @@ package com.ousl.transgas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateActivity extends AppCompatActivity {
-
+    private Button createButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        createButton = findViewById(R.id.createButton);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreateActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/ousl/transgas/LoginActivity.java
+++ app/src/main/java/com/ousl/transgas/LoginActivity.java
@@ -2,13 +2,28 @@ package com.ousl.transgas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
