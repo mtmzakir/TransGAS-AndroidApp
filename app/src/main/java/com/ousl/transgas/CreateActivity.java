55045c5ee99be1c382ddd3fe0e7d5c6273242b16
zakir <mtm.zakir@gmail.com>
2023-04-17 05:14:45 +0530

    13-Fixed Register Page

--- app/src/main/java/com/ousl/transgas/CreateActivity.java
+++ app/src/main/java/com/ousl/transgas/CreateActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class CreateActivity extends AppCompatActivity {
     Button createButton;
+    ImageButton googleButton, facebookButton;
     EditText name, email , address , mobileNumber, password;
     TextView loginButton;
     DBHelper DB;
@@ -28,6 +30,8 @@ public class CreateActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.password);
         createButton = findViewById(R.id.createButton);
         loginButton = findViewById(R.id.loginButton);
+        googleButton = findViewById(R.id.google_Button);
+        facebookButton = findViewById(R.id.facebook_Button);
         DB = new DBHelper(this);
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,20 +43,20 @@ public class CreateActivity extends AppCompatActivity {
                 String passwords = password.getText().toString();
 
                 if (names.equals("") || emails.equals("") || addresses.equals("") || mobileNumbers.equals("") || passwords.equals(""))
-                    Toast.makeText(CreateActivity.this, "Enter All Fields", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(CreateActivity.this, "Fields Cannot Be Empty ❗", Toast.LENGTH_SHORT).show();
                 else {
                     Boolean checkemail = DB.checkemail(emails);
                     if (checkemail==false) {
                         Boolean insert = DB.insertData(names,emails,addresses,mobileNumbers,passwords);
                         if (insert==true) {
-                            Toast.makeText(CreateActivity.this, "Registation Successful", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(CreateActivity.this, "Account Create Success \uD83D\uDE0A", Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(CreateActivity.this,LoginActivity.class);
                             startActivity(intent);
                         } else {
-                            Toast.makeText(CreateActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(CreateActivity.this, "Registration Failed ⚠", Toast.LENGTH_SHORT).show();
                         }
                     }
-                    else Toast.makeText(CreateActivity.this, "User Already Exist", Toast.LENGTH_SHORT).show();
+                    else Toast.makeText(CreateActivity.this, "User Already Exist ❗", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -60,8 +64,28 @@ public class CreateActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                openLoginPage();
             }
         });
+
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openToastNotAvailable();
+            }
+        });
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openToastNotAvailable();
+            }
+        });
+    }
+    public void openLoginPage() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void openToastNotAvailable() {
+        Toast.makeText(this, "Feature Unavailable \uD83D\uDE41", Toast.LENGTH_SHORT).show();
     }
 }
