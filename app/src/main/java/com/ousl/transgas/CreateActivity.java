d7ae5b6570e14ed9c27644942bff5ae26a7a7995
zakir <mtm.zakir@gmail.com>
2023-04-17 01:16:01 +0530

    8

--- app/src/main/java/com/ousl/transgas/CreateActivity.java
+++ app/src/main/java/com/ousl/transgas/CreateActivity.java
@@ -6,21 +6,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateActivity extends AppCompatActivity {
-    private Button createButton;
+    Button createButton;
+    EditText name, email , address , mobileNumber, password;
+    TextView loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
+        name = (EditText) findViewById(R.id.name);
+        email = (EditText) findViewById(R.id.email);
+        address = (EditText) findViewById(R.id.address);
+        mobileNumber = (EditText) findViewById(R.id.mobileNumber);
+        password = (EditText) findViewById(R.id.password);
+
         createButton = findViewById(R.id.createButton);
+        loginButton = findViewById(R.id.loginButton);
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(CreateActivity.this,LoginActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
---
+++ app/src/main/java/com/ousl/transgas/DBHelper.java
@@ -0,0 +1,26 @@
+package com.ousl.transgas;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DBNAME = "Login.db";
+
+    public DBHelper(Context context) {
+        super(context, "Login.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase MyDB) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase MyDB, int i, int i1) {
+
+    }
+}
